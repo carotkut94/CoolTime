e5133fe7649d620d81922c8e9946fa2ece3c6f22
DeathCode <carotkut12@gmail.com>
2018-04-18 18:30:59 +0530

    added all the required fields for standard unix epoch to human time

--- mainapp.py
+++ mainapp.py
@@ -6,15 +6,31 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/live')
+def live():
+    return 'Yes I am alive!'
 
 
 @app.route('/cooltime')
 def get():
+    # get request parameter
     time = request.args.get('time')
-    return jsonify({"Date": datetime.datetime.fromtimestamp(int(time)).strftime('%d-%m-%Y %H:%M:%S')})
+
+    # temp date to store
+    temp = datetime.datetime.fromtimestamp(int(time)).strftime('%d-%m-%Y %H:%M:%S')
+    date = datetime.datetime.strptime(temp, '%d-%m-%Y %H:%M:%S')
+
+    # return date time data as json payload
+    return jsonify({"Date": date,
+                    "YearDay": date.timetuple().tm_yday,
+                    "WeekDay": date.timetuple().tm_wday,
+                    "Hour": date.timetuple().tm_hour,
+                    "Minute": date.timetuple().tm_min,
+                    "Seconds": date.timetuple().tm_sec,
+                    "Day": date.timetuple().tm_mday,
+                    "Month": date.timetuple().tm_mon,
+                    "Year": date.timetuple().tm_year
+                    })
 
 
 if __name__ == '__main__':
